f723934e4840d314230585761ccb76ab25801bf4
switchtrip <aswitchtrip@gmail.com>
2024-04-14 22:31:33 +0530

    Working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,3 @@
-<body class="min-h-screen grid gap-6 content-center justify-center justify-items-center">
-<h1 class="font-serif text-9xl text-center ">Random bg</h1>
-<button class="font-sans text-4xl font-black text-white bg-black hover:shadow-md hover:shadow-black transition duration-200 rounded-full p-9 my-7 w-max h-max" on:click={cbg}>
-        Try!!
-</button>
-</body>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
-
 <script>
     function gRI(max) {
         return Math.floor(Math.random() * max);
@@ -26,6 +13,21 @@
         const [c2, c2x] = gRC();
         this.style.background = `linear-gradient(to bottom, ${c1}, 90%, ${c2})`;
         this.style.color = `${c2}`;
-        document.body.style.background =`rgb(${gRI(255)} ${gRI(255)} ${gRI(255)})`; 
+        const main = document.querySelector("main");
+        if (main)
+        main.style.backgroundColor =`rgb(${gRI(255)} ${gRI(255)} ${gRI(255)})`; 
     }
 </script>
+
+<main class="w-screen h-screen grid gap-6 content-center bg-slate-600 justify-center justify-items-center">
+<h1 class="font-serif text-9xl text-center ">Random bg</h1>
+<button class="font-sans text-4xl font-black text-white bg-black hover:shadow-md hover:shadow-black transition duration-200 rounded-full p-9 my-7 w-max h-max" on:click={cbg}>
+        Try!!
+</button>
+</main>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
