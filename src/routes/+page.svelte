e01a7f74b2cb079c9f774fa30b4ffc6ef1b573af
switchtrip <aswitchtrip@gmail.com>
2024-04-15 16:42:41 +0530

    Add blob

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,13 @@
   let btnBg = `rgb(100, 100, 100)`;
   let btnFg = `rgb(0, 0, 0)`;
   let mainBg = `rgb(200, 200, 200)`;
-
-    function gRI(max) {
-        return Math.floor(Math.random() * max);
+  let dataBorder = getRadius(gRI(100), gRI(100),gRI(100), gRI(100));
+  let blobTop = "30%";
+  let blobRight = "30%";
+  let blobH = "100px";
+  let blobW = "100px";
+    function gRI(max=100, min=0) {
+        return Math.floor(Math.random() * (max-min)+min);
     }
     function gRC(){
         const r = gRI(255);
@@ -17,8 +21,16 @@
         const [c2, c2x] = gRC();
         const [c3, c3x] = gRC();
         btnBg = `linear-gradient(to bottom, ${c1}, 90%, ${c2})`;
-        btnFg = `${c2}`;
+        btnFg = `${c1x}`;
         mainBg = c3;
+        dataBorder = getRadius(gRI(80,20), gRI(80, 20),gRI(80, 20), gRI(80, 20));
+        blobRight = `${gRI(80)}%`;
+        blobTop = `${gRI(80)}%`;
+        blobH = `${gRI(400, 50)}px`;
+        blobW = `${gRI(400, 50)}px`;
+    }
+    function getRadius(a, b, c, d){
+      return `${a}% ${100-a}% ${b}% ${100-b}% / ${c}% ${100-d}% ${d}% ${100-c}%`;
     }
 </script>
 
@@ -27,10 +39,19 @@
 <button style="background:{btnBg}; color:{btnFg}" class="font-sans text-4xl font-black text-white bg-black hover:shadow-md hover:shadow-black transition duration-200 rounded-full p-9 my-7 w-max h-max" on:click={cbg}>
         Try!!
 </button>
+<div class="bubble" style="width:{blobW}; height:{blobH}; background:{btnBg}; top:{blobTop}; right:{blobRight}; border-radius:{dataBorder}"></div>
 </main>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
+<style>
+  .bubble{
+    position: fixed;
+    width:180px;
+    height:280px;
+    background-color: crimson;
+    border-radius: attr(data-border);
+    opacity: 0.5;
+  }
+  button{
+    z-index: 999;
   }
 </style>
