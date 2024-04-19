dbe273e7cb227d507e2b912607e80c8a57cc236b
switchtrip <aswitchtrip@gmail.com>
2024-04-19 20:13:00 +0530

    Trying gh fix

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
   import "../app.pcss";
+  import { base } from '$app/paths';
 </script>
 
 <nav class="flex items-center justify-center gap-5 p-4 text-white bg-gradient-to-r from-slate-900 to-slate-500">
-  <a href="/">Home</a>
-  <a href="/gallery">Gallery</a>
+  <a href="{base}/">Home</a>
+  <a href="{base}/gallery">Gallery</a>
 </nav>
 
 <div class="bg-[#0e172a] min-h-screen pb-20">
