19804e995d35e2733c53b99ea9c27d200068fd23
switchtrip <aswitchtrip@gmail.com>
2024-04-27 08:56:51 +0530

    Remove old gallery

--- src/routes/old_gallery/+page.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export async function load({fetch}) {
-    const resp = await fetch('data/gallery.json');
-    const data = await resp.json();
-    return {"gallery": data};
-};
-// export const ssr = false;
