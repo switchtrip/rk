8c5b25c8c021e632c20abdc2c57e0208f48c09eb
switchtrip <aswitchtrip@gmail.com>
2024-05-04 16:17:01 +0530

    Clean event page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -10,18 +10,4 @@
     <h1 class="h1">Events</h1>
   </div>
 
-  <!-- <div class="max-w-96 border-2 border-white p-4 grid grid-rows-3 justify-items-center"> -->
-  <div
-    class="max-w-96 p-4 flex flex-col gap-10 items-center"
-  >
-    <p class="text-sm space-x-3">
-      <span>Mouth Organ </span>
-      <span>Flute </span>
-      <span>Classical </span>
-    </p>
-    <h1 class="font-[Palatine] text-6xl text-[#edaa40] leading-none">Surabhi</h1>
-    <h4 class="text-lg text-[#edaa40]/40 -mt-10 leading-3">Music Academy</h4>
-    <img src="{base}/images/seprator_flute.png" alt="Flute"/>
-    <h2 class="font-bold text-primary-600 -mt-5">Shri Sunil Patil</h2>
-  </div>
 </div>
